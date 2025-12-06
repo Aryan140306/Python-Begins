06b322c4c138993c5764b11b5e7a421a3a4b84c2
Aryan140306 <aryanverma140306@gmail.com>
2025-12-06 11:17:56 +0530

    Refactor character input checks and outputs

--- que 4.py
+++ que 4.py
@@ -1,25 +1,12 @@
-char = input("Enter a character: ")
-
-if len(char) != 1:
-    print("Please enter exactly one character!")
-else:
-    # a. Check type of character
-    if char.isalpha():  # if it's a letter
-        print("It is a letter.")
-        
-        # b. Check case
-        if char.isupper():
-            print("The letter is uppercase.")
-        else:
-            print("The letter is lowercase.")
-    
-    elif char.isdigit():  # if it's a digit
-        print("It is a numeric digit.")
-        
-        # c. Print its name
-        digit_names = ["ZERO", "ONE", "TWO", "THREE", "FOUR", 
-                       "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"]
-        print("Its name is:", digit_names[int(char)])
-    
+char=input("enter the character")
+if char.isalpha():
+    print("it is a letter")
+    if char.isupper():
+        print('the letter is uppercase')
     else:
-        print("It is a special character.")
+        print('the letter is lowercase')
+elif char.isdigit():
+    print('it is a numeric digit')
+else:
+    print('it is a special character')
+    
